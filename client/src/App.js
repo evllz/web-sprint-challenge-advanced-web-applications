637c2881a10afa99c9648330da6be87455a412fa
unknown <erickvillegasecheverria@gmail.com>
2020-12-11 20:36:53 -0800

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
+
+//username: Lambda School
+//password: i<3Lambd4
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute exact path="/bubble-page" component={BubblePage} />
       </div>
     </Router>
   );
